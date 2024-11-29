6cafb388d778d6325a4e64b4a67be0afab5db572
Acintya Edria Sudarsono <150018995+Aceeen@users.noreply.github.com>
2024-11-29 21:14:00 +0700

    Create HomeView.vue

---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Selamat datang di Perpustakaan</h1>
+    <BookList />  <!-- Menampilkan komponen BookList untuk daftar buku -->
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BookList from '../components/BookList.vue'; // Import komponen BookList
+
+export default defineComponent({
+  name: 'HomeView',
+  components: {
+    BookList
+  }
+});
+</script>
+
+<style scoped>
+/* Kamu bisa menambahkan gaya khusus untuk halaman beranda di sini */
+</style>
