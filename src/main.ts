e2e240412d4e6057223897448e200e91c1b5d9f5
nopiii <cahganggu@gmail.com>
2024-11-29 17:58:14 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import './assets/main.css'; //
 
+// Create the Vue application
 createApp(App)
-  .use(router)
-  .mount('#app');
+  .use(router)  // Register Vue Router
+  .mount('#app'); // Mount the app to the DOM
+
